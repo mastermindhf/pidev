b5d751131173656088a797d9a783bdc1b8d849dc
elyes1908 <elyes.tarmiz@esprit.tn>
2020-02-25 00:35:35 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/commandlinetools/Composer_10_02_2020_21_54_c.xml
    #	.idea/commandlinetools/Symfony_10_02_2020_21_54_s.xml
    #	app/AppKernel.php
    #	app/Resources/views/Back/back.html.twig
    #	app/Resources/views/login.html.twig
    #	app/config/config.yml
    #	app/config/routing.yml

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use CantineBundle\Entity\Notification;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,9 +14,11 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        $user = $this->container->get('security.token_storage')->getToken()->getUser()->getId();
+        $notifs=$this->getDoctrine()->getRepository(Notification::class)->findAll();
+        $x=count($notifs);
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,'u'=>$user,'x'=>$x
         ]);
     }
 }
--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace UserBundle\Entity;
+use FOS\MessageBundle\Model\ParticipantInterface;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -10,7 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="user")
  * @ORM\Entity(repositoryClass="UserBundle\Repository\UserRepository")
  */
-class User extends BaseUser
+class User extends BaseUser implements ParticipantInterface
 {
     /**
      * @var int
@@ -20,6 +21,30 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+    /**
+     * @ORM\ManyToOne(targetEntity="SuiviBundle\Entity\Classe" )
+     * @ORM\JoinColumn(name="id_c", referencedColumnName="id")
+     */
+
+    private $classe;
+
+    /**
+     * @return mixed
+     */
+    public function getClasse()
+    {
+        return $this->classe;
+    }
+
+    /**
+     * @param mixed $classe
+     * @return User
+     */
+    public function setClasse($classe)
+    {
+        $this->classe = $classe;
+        return $this;
+    }
 
     /**
      * @var string
@@ -27,6 +52,49 @@ class User extends BaseUser
      * @ORM\Column(name="nom", type="string", nullable=false)
      */
     private $nom;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prenom", type="string", nullable=false)
+     */
+    private $prenom;
+
+    /**
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param string $prenom
+     * @return User
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+        return $this;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
 
     /**
      * @return string
@@ -35,6 +103,15 @@ class User extends BaseUser
     {
         return $this->nom;
     }
+
+    /**
+     * @return array
+     */
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
 
     /**
      * @param string $nom
